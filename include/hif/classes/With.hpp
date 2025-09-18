de3d5da7a05e52697ee43571e10c14d2c34a4da9
Galfurian <enrico.fraccaroli@univr.it>
2025-09-18 14:35:10 -0400

    documentation(doxygen): add missing @param and @return to With methods
    - Added @param and @return documentation to _getFieldName and _getBListName methods
    - Fixed 4 Doxygen warnings in With.hpp

--- include/hif/classes/With.hpp
+++ include/hif/classes/With.hpp
@@ -86,9 +86,13 @@ protected:
     virtual void _calculateFields();
 
     /// @brief Returns the name of given child w.r.t. this.
+    /// @param child The child object.
+    /// @return The name of the child.
     virtual std::string _getFieldName(const Object *child) const;
 
     /// @brief Returns the name of given BList w.r.t. this.
+    /// @param list The BList to get the name for.
+    /// @return The name of the BList.
     virtual std::string _getBListName(const BList<Object> &list) const;
 
 private:

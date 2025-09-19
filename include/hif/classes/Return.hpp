80307a72f395f4c8e1b0a8f53ddb59ffb0745ff2
Enrico Fraccaroli <enrico.fraccaroli@univr.it>
2025-09-19 10:37:43 -0400

    documentation(doxygen): remove redundant documentation from virtual function overrides

--- include/hif/classes/Return.hpp
+++ include/hif/classes/Return.hpp
@@ -54,11 +54,6 @@ protected:
     /// @brief Fills the internal fields and blists lists.
     virtual void _calculateFields();
 
-    /**
-     * @brief Returns the name of given child w.r.t. this.
-     * @param child The child object to get the name for.
-     * @return The name of the child.
-     */
     virtual std::string _getFieldName(const Object *child) const;
 
 private:

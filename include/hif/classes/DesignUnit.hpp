d6be73a33400de067913f41c7e15a3ed6c5a4beb
Enrico Fraccaroli <enrico.fraccaroli@univr.it>
2025-09-19 10:23:22 -0400

    documentation(doxygen): add @param and @return to _getBListName in DesignUnit.hpp

--- include/hif/classes/DesignUnit.hpp
+++ include/hif/classes/DesignUnit.hpp
@@ -49,7 +49,11 @@ protected:
     /// @brief Fills the internal fields and blists lists.
     virtual void _calculateFields();
 
-    /// @brief Returns the name of given BList w.r.t. this.
+    /**
+     * @brief Returns the name of given BList w.r.t. this.
+     * @param list The BList to get the name for.
+     * @return The name of the BList.
+     */
     virtual std::string _getBListName(const BList<Object> &list) const;
 };
 

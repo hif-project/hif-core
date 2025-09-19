000e7469b82b7aeb67b7399d202dd697bf9b38a7
Enrico Fraccaroli <enrico.fraccaroli@univr.it>
2025-09-19 09:48:21 -0400

    documentation(doxygen): add missing @param and @return documentation in IfGenerate.hpp

--- include/hif/classes/IfGenerate.hpp
+++ include/hif/classes/IfGenerate.hpp
@@ -58,6 +58,8 @@ protected:
     virtual void _calculateFields();
 
     /// @brief Returns the name of given child w.r.t. this.
+    /// @param child The child object.
+    /// @return The name of the child.
     virtual std::string _getFieldName(const Object *child) const;
 
 private:
